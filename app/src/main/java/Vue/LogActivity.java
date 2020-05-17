0eb6d3156a9a194738815096f13b0fd0270910dc
Cedric <cedricpasquierpro@gmail.com>
2020-05-17 11:49:34 +0200

    New activity with the recycler list

--- app/src/main/java/Vue/LogActivity.java
+++ app/src/main/java/Vue/LogActivity.java
@@ -1,23 +1,26 @@
 package Vue;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.leguidebienpnard.R;
-import com.google.firebase.auth.FirebaseAuth;
+import androidx.annotation.NonNull;
 
-import org.json.JSONException;
+import com.example.leguidebienpnard.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import Controler.Authentification;
 
 
 public class LogActivity extends BaseActivity implements View.OnClickListener{
@@ -26,111 +29,74 @@ public class LogActivity extends BaseActivity implements View.OnClickListener{
     private ProgressBar progressBar;
     public EditText editTextEmail;
     public EditText editTextMDP;
+    public EditText editTextMDP2;
     private FirebaseAuth mAuth;
     private Button buttonSwitch;
-
-    final Toast toast[] = new Toast[1];
-
-    public Authentification authentification;
+    private TextView textViewMode;
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    Toast toast;
+    private static final String TAG = "EmailPassword";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.log_main);
         setProgressBar(R.id.progressBar);
-
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        // View
+        textViewMode = findViewById(R.id.textViewMode);
         // Views
-        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
-        editTextMDP = (EditText) findViewById(R.id.editTextMDP);
-
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextMDP = findViewById(R.id.editTextMDP);
+        editTextMDP2 = findViewById(R.id.editTextMDP2);
 
         //Buttons
         buttonSwitch = (Button) findViewById(R.id.buttonSwich);
-        findViewById(R.id.buttonSignedIn).setOnClickListener(this);
+        findViewById(R.id.buttonValider).setOnClickListener(this);
         findViewById(R.id.buttonCreateAccount).setOnClickListener(this);
         buttonSwitch.setOnClickListener(this);
+
+        toast = new Toast(this);
+
         mAuth = FirebaseAuth.getInstance();
+        //authentification = new Authentification(getApplicationContext());
 
-        try {
-            authentification = new Authentification(mAuth);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
     }
-
-
-    private void updateUI() {
-        hideProgressBar();
-    }
-
 
     @Override
     public void onClick(View v){
         int i = v.getId();
-        if(i == R.id.buttonSignedIn){
-            switch(authentification.signIn(editTextEmail.getText().toString(), editTextMDP.getText().toString())){
-                case "success":
-                    if(toast[0]!=null){
-                        toast[0].cancel();
-                    }
-                    toast[0].makeText(getApplicationContext(), "Authentication réussi.",
-                            Toast.LENGTH_SHORT).show();
-                    updateUI();
-                    break;
-                case "email":
-                    if(toast[0]!=null){
-                        toast[0].cancel();
-                    }
-                    toast[0].makeText(getApplicationContext(), "Votre adresse email doit être vérifier.",
-                            Toast.LENGTH_SHORT).show();
-                    updateUI();
-                    break;
-                case "fail":
-                    // If sign in fails, display a message to the user.
-                    if(toast[0]!=null){
-                        toast[0].cancel();
-                    }
-                    toast[0].makeText(getApplicationContext(), "L'authentication a échoué.",
-                            Toast.LENGTH_SHORT).show();
-                    updateUI();
-                    break;
-                default:
-
+        if(i == R.id.buttonValider){ // action lors d'un click sur le bouton validé
+            if (!validateForm()) {
+                return;
             }
-        } else if (i == R.id.buttonCreateAccount) {
-            switch(authentification.createAccount(editTextEmail.getText().toString(), editTextMDP.getText().toString())){
-                case "success":
-                    // Sign in success, update UI with the signed-in user's information
-                    if(toast[0]!=null){
-                        toast[0].cancel();
-                    }
-                    toast[0].makeText(getApplicationContext(), "Compte créé, veuillez verifier l'email",
-                            Toast.LENGTH_SHORT).show();
-                    updateUI();
-                    break;
-                case "fail":
-                    if(toast[0]!=null){
-                        toast[0].cancel();
-                    }
-                    toast[0].makeText(getApplicationContext(), "La création du compte a échoué",
-                            Toast.LENGTH_SHORT).show();
-                    updateUI();
-                    break;
-                default:
-            };
-
+            signIn(editTextEmail.getText().toString(), editTextMDP.getText().toString());
+        } else if (i == R.id.buttonCreateAccount) { // action lors d'un click sur le bouton créer
+            if (!validateForm()) {
+                return;
+            }
+            if(!editTextMDP.equals(editTextMDP2)){
+                editTextMDP.setError("Veuillez entrer le même mot de passe");
+                return;
+            }
+            createAccount(editTextEmail.getText().toString(), editTextMDP.getText().toString());
+            FirebaseUser user = mAuth.getCurrentUser();
+            updateUI(user);
             buttonSwitch.setText("Créer un compte");
-            findViewById(R.id.buttonSignedIn).setVisibility(View.VISIBLE);
+            findViewById(R.id.buttonValider).setVisibility(View.VISIBLE);
             findViewById(R.id.buttonCreateAccount).setVisibility(View.GONE);
         } else if(i == R.id.buttonSwich){
             if(buttonSwitch.getText().toString().equals("Créer un compte")){
                 buttonSwitch.setText("S'authentifier");
-                findViewById(R.id.buttonSignedIn).setVisibility(View.GONE);
+                textViewMode.setText("Créer un compte");
+                findViewById(R.id.buttonValider).setVisibility(View.GONE);
+                findViewById(R.id.tableRowMDP2).setVisibility(View.VISIBLE);
                 findViewById(R.id.buttonCreateAccount).setVisibility(View.VISIBLE);
             } else if(buttonSwitch.getText().toString().equals("S'authentifier")){
                 buttonSwitch.setText("Créer un compte");
-                findViewById(R.id.buttonSignedIn).setVisibility(View.VISIBLE);
+                textViewMode.setText("Connexion");
+                findViewById(R.id.buttonValider).setVisibility(View.VISIBLE);
+                findViewById(R.id.tableRowMDP2).setVisibility(View.GONE);
                 findViewById(R.id.buttonCreateAccount).setVisibility(View.GONE);
             }
         }
@@ -149,11 +115,140 @@ public class LogActivity extends BaseActivity implements View.OnClickListener{
         return false;
     }
 
+
+    private void updateUI(FirebaseUser user){
+        if(user != null){
+            Intent intent = new Intent(this, MainActivity.class);
+            String[] userAdrress = user.getEmail().split("@");
+            String userName = userAdrress[0];
+            intent.putExtra(EXTRA_MESSAGE, userName);
+            startActivity(intent);
+        }
+        hideProgressBar();
+        editTextEmail.setText("");
+        editTextEmail.requestFocus();
+        editTextMDP.setText("");
+        editTextMDP2.setText("");
+
+    }
+
+    protected void connexionSucess(){
+        toast.makeText(getApplicationContext(), "Authentication réussi.",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    protected void verificationNeeded(){
+        toast.makeText(getApplicationContext(), "Votre adresse email doit être vérifier.",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    protected void connexionFail(){
+        toast.makeText(getApplicationContext(), "L'authentication a échoué.",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    protected void accountCreated(){
+        toast.makeText(getApplicationContext(), "Compte créé, veuillez verifier l'email",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    protected void creationFailed(){
+        toast.makeText(getApplicationContext(), "La création du compte a échoué",
+                Toast.LENGTH_SHORT).show();
+    }
+
+
+
     public static boolean isEmailValid(String email) {
         String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
         Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(email);
         return matcher.matches();
+    }
+
+
+
+    public void signIn(String email, String password){
+        showProgressBar();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            if(mAuth.getCurrentUser().isEmailVerified()){
+                                Log.d(TAG, "signInWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                updateUI(user);
+                            } else {
+                                verificationNeeded();
+                            }
+                        } else {
+                            Log.w(TAG, "signInWithEmail:failure",task.getException());
+                            connexionFail();
+                        }
+
+                        // [START_EXCLUDE]
+                        hideProgressBar();
+                        // [END_EXCLUDE]
+                    }
+                });
+    }
+
+    public void createAccount(String email, String password) {
+        showProgressBar();
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "createUserWithEmail:success");
+                            sendEmailVerification();
+                            accountCreated();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            creationFailed();
+                        }
+                        // [START_EXCLUDE]
+                        hideProgressBar();
+                        // [END_EXCLUDE]
+                    }
+                });
+    }
+
+    public void sendEmailVerification() {
+
+        // Send verification email
+        // [START send_email_verification]
+        final FirebaseUser user = mAuth.getCurrentUser();
+        user.sendEmailVerification()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // [START_EXCLUDE]
+                        // Re-enable button
+
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "sendingEmail:success");
+                        } else {
+                            Log.e(TAG, "sendEmailVerification", task.getException());
+                        }
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END send_email_verification]
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        mAuth.signOut();
     }
 
 }
---
+++ app/src/main/java/Vue/MainActivity.java
@@ -0,0 +1,41 @@
+package Vue;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.leguidebienpnard.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Intent intent = getIntent();
+        String userName = intent.getStringExtra(LogActivity.EXTRA_MESSAGE);
+
+        Toast.makeText(this,userName, Toast.LENGTH_LONG).show();
+    }
+
+}
